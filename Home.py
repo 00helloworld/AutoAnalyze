f1f1e977772e7b3303d4e6a3a1070922a49270d2
yanshen <yanshen0406@gmail.com>
2024-04-03 17:38:27 -0500

    prettify

--- Home.py
+++ Home.py
@@ -16,5 +16,13 @@ st.markdown(
     </style>
     """, unsafe_allow_html=True
 )
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+
 
 st.write('# Welcome!')
--- pages/0_About.py
+++ pages/0_About.py
@@ -16,6 +16,14 @@ st.markdown(
     </style>
     """, unsafe_allow_html=True
 )
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+
 
 st.write('# About')
 st.write('Here will be updated with how to use and new features.')
